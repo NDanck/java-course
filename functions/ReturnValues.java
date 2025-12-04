8bcdc05a21d048d8a4ceb070a764dcca1cdff1bb
NDanck <nicolasdanc3@gmail.com>
2025-12-04 20:06:21 -0300

    Void functions & value returning functions

--- functions/ReturnValues.java
+++ functions/ReturnValues.java
@@ -13,8 +13,13 @@ public class ReturnValues {
         System.out.println("Calculating...");
         double calculatedArea = calculateArea(lenght, width);
 
-        System.out.println("Calculated area: " + calculatedArea + "m^2");
-        
+        printArea(lenght, width, calculatedArea);
+
+        System.out.println("What language do you speak? ");
+        scan.nextLine();
+        String language = scan.nextLine();
+        System.out.println(explainArea(language));
+
         scan.close();
     }
 
@@ -22,5 +27,19 @@ public class ReturnValues {
         double area = lenght * width;
 
         return area;
+    }
+
+    public static String explainArea(String language) {
+
+        switch (language) {
+            case "English": return "Area equals lenght * width";
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "Area es igual a largo * ancho";
+            default: return "Unsupported language :(";
+        }
+    }
+
+    public static void printArea(double lenght, double width, double area) {
+        System.out.println("A rectangle with a lenght of " + lenght + " and a wodth of " + width + " has an area of " + area + "m^2");
     }
 }
